33a2685df3c6d171f2fc95e2e9b4be173a9b50a1
Scott Odle <scott@sjodle.com>
2024-05-14 07:15:45 -0600

    fix merge

--- zsh/config/000-paths.zsh
+++ zsh/config/000-paths.zsh
@@ -3,3 +3,9 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # Homebrew
 export PATH="/opt/homebrew/bin:$PATH"
+
+# Cargo
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Bob-NVIM
+export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
