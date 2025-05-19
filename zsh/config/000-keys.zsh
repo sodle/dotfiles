3588cffdc1232474f180067aa9311c787da98a9f
Scott Odle <scott@sjodle.com>
2025-05-18 20:03:12 -0600

    bash-style words

--- zsh/config/000-keys.zsh
+++ zsh/config/000-keys.zsh
@@ -1,2 +1,8 @@
 KEYMAP=emacs
 bindkey -A $KEYMAP main
+
+# Use bash-style word boundaries, 
+# where each component of a path is considered its own word, 
+# instead of the whole path being one word
+autoload -U select-word-style
+select-word-style bash
